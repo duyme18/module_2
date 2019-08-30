e72e1393238e86c0598d97a9f7a388f1e9af09f8
HDD <duyme18@gmail.com>
2019-08-30 08:45:35 +0700

    [Thực hành] Tìm ước số chung lớn nhất

--- TASK404314.java
+++ TASK404314.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class TASK404314 {
+    public static void main(String[]args){
+        int a,b;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter a:");
+        a=scanner.nextInt();
+        System.out.println("Enter b:");
+        b=scanner.nextInt();
+        a=Math.abs(a);
+        b=Math.abs(b);
+        if(a==0||b==0){
+            System.out.println("No greate comon factor");
+        }
+        while (a != b) {
+            if (a > b)
+                a = a - b;
+            else
+                b = b - a;
+        }
+        System.out.println("Greatest common factor: " + a);
+    }
 }
