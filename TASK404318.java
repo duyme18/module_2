f2a1c957434d698ceee3535f5fab08ffa7be1f0d
HDD <duyme18@gmail.com>
2019-08-30 08:47:50 +0700

    [*Bài tập] Hiển thị các số nguyên tố nhỏ hơn 100

--- TASK404318.java
+++ TASK404318.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class TASK404318 {
+    public static void main(String[] args) {
+        int i, j;
+        int check = 1;
+        for (i = 2; i <= 100; i++) {
+            for (j = 2; j <= i - 1; j++) {
+                if (i % j == 0) {
+                    check = 0;
+                }
+            }
+            if (check == 1) {
+                System.out.printf("%d\t", i);
+            }
+            check = 1;
+        }
+    }
 }
