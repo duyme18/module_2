f131e996ec41a7258551b1258a0708c09ac52952
HDD <duyme18@gmail.com>
2019-08-30 08:48:27 +0700

    [**Bài tập] Hiển thị 20 số nguyên tố đầu tiên

--- TASK404319.java
+++ TASK404319.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class TASK404319 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        int i, j;
+        int check = 1;
+        for (i = 2; i <= number; i++) {
+            for (j = 2; j <= i - 1; j++) {
+                if (i % j == 0) {
+                    check = 0;
+                }
+            }
+            if (check == 1) {
+                System.out.printf("%d\t", i);
+            }
+            check = 1;
+        }
+    }
 }
