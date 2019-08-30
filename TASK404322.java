b379cec8c5207fbe7360b7b3a70d9e629c2b93be
HDD <duyme18@gmail.com>
2019-08-30 08:49:58 +0700

    [*Bài tập] Ứng dụng chuyển đổi tiền tệ

--- TASK404322.java
+++ TASK404322.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class TASK404322 {
+    public static void main(String[] args) {
+        double vnd = 23000;
+        double usd;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập số tiền USD: ");
+        usd = scanner.nextDouble();
+        double result = usd * vnd;
+        System.out.print(usd + " USD = " + result + " VND");
+    }
 }
