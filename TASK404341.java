51f4c11d4a5ba086cd50fc5dd167bd9c09bda97b
HDD <duyme18@gmail.com>
2019-09-02 00:14:50 +0700

    [Bài tập] Thêm phần tử vào mảng

--- TASK404341.java
+++ TASK404341.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import javafx.css.Size;
+
+import java.util.Scanner;
 
 public class TASK404341 {
+    public static void main(String[] args) {
+        int x, k;
+        int[] arr = new int[10];
+        arr[0] = 10;
+        arr[1] = 4;
+        arr[2] = 6;
+        arr[3] = 7;
+        arr[4] = 8;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập giá trị cần chèn, x = ");
+        x = sc.nextInt();
+
+        System.out.print("Nhập vị trí cần chèn, k = ");
+        k = sc.nextInt();
+        if (k > 1 && k <= arr.length - 1) {
+            for (int i = 0; i < arr.length; i++) {
+                if (k == i) {
+                    arr[i] = arr[i + 1];
+                    arr[i] = x;
+                }
+            }
+        }
+        for (int a : arr)
+            System.out.println("các phần tử sau khi chèn: " + a);
+    }
+
+
 }
+
