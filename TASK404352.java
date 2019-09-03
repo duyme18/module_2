cd89bdfc1b3b03818b534d2d9de059e8f8800aa9
HDD <duyme18@gmail.com>
2019-09-04 00:04:10 +0700

    [*Bài tập] Đếm số lần xuất hiện của ký tự trong chuỗi

--- TASK404352.java
+++ TASK404352.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class TASK404352 {
+    public static void main(String[] args) {
+        String str;
+        char kyTu = 'a';
+        int count = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Nhập vào chuỗi bất kỳ: ");
+        str = scanner.nextLine();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == kyTu) {
+                count++;
+            }
+        }
+        System.out.println("Số lần xuất hiện của ký tự " + kyTu + " trong chuỗi " + str + " = " + count);
+    }
 }
