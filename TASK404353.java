40dfdd20c8ef97c6e888112996b86a9832ec0ccc
HDD <duyme18@gmail.com>
2019-09-03 23:11:31 +0700

    [*Bài tập] Mảng hai chiều - tính tổng các số ở đường chéo chính của ma trận vuông

--- TASK404353.java
+++ TASK404353.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
-
 public class TASK404353 {
+    public static void main(String[] args) {
+        int[][] a = new int[3][3];
+        a[0][0] = 1;
+        a[0][1] = 2;
+        a[0][2] = 3;
+        a[1][0] = 4;
+        a[1][1] = 5;
+        a[1][2] = 6;
+        a[2][0] = 7;
+        a[2][1] = 8;
+        a[2][2] = 9;
+        display(a);
+        Sum(a);
+    }
+
+    public static void display(int[][] a) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    public static void Sum(int[][] a) {
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                if (i == j) {
+                    sum += a[i][j];
+                }
+            }
+        }
+        System.out.println("Tổng đường chéo chính = " + sum);
+    }
 }
